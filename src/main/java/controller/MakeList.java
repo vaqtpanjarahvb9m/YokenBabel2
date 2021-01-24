260f6118542a07e29586138706646ee6c9372e3e
vaqtpanjarahvb9m <wangwei340@yeah.net>
2021-01-24 20:27:37 +0800

    no message

---
+++ src/main/java/controller/MakeList.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author anonybook
+ */
+public class MakeList {
+    
+    private ArrayList<String> list;
+    private String classe;
+    
+    private ArrayList<String> getList(){
+        
+        return list;
+        
+    }
+    private void setList(ArrayList list){
+        
+        this.list = list;
+        
+    }
+           
+    public MakeList() {
+        readClasses();
+    }
+    
+    public void readClasses() {
+        
+        
+        
+    }
+    
+}
